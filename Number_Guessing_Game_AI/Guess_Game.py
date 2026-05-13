82d93240b400bb5b09e4a0a7ade63a5ff714890e
Anu-yadav923 <anuy8306@gmail.com>
2026-05-13 20:18:29 +0530

    Completed Guessing Game

--- Number_Guessing_Game_AI/Guess_Game.py
+++ Number_Guessing_Game_AI/Guess_Game.py
@@ -1,13 +1,34 @@
 import random
-Secret_number = random.randint(1,10)
 
-while(True):
-    guess_number = int(input("Guess a number between 1 to 10: "))
+print("welcome to Guessing Game!!")
+
+Difficulty_level = int(input("choose difficulty level between 1 to 3 : "))
+
+if Difficulty_level == 1:
+    Secret_number = random.randint(1,10)
+elif Difficulty_level == 2:
+    Secret_number = random.randint(1,50)
+
+else :
+    Secret_number = random.randint(1,100)
+
+attempts = 0
+won = False
+
+while attempts < 5:
+    guess_number = int(input("Guess a number: "))
+    attempts += 1
 
     if guess_number == Secret_number:
         print("yee!! You guessed the correct number!")
+        print("attempts : ", attempts)
+        won = True
         break
     elif guess_number > Secret_number:
         print("Too high, Guess Smaller number")
     else:
         print("Too low, Guess Larger number.")
+
+if won == False:
+    print("Game Over")
+    print(f"correct Number was : {Secret_number}")
