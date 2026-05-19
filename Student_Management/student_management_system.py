c124f22719ed865a203aee8f9f322de2fa0c4d85
Anu-yadav923 <anuy8306@gmail.com>
2026-05-20 01:19:54 +0530

    updated student_management_system part 1

--- Student_Management/student_management_system.py
+++ Student_Management/student_management_system.py
@@ -20,7 +20,9 @@ students = []
 print("/n Welcome to student management system")
 print("1. Add student")
 print("2. View student")
-print("3. Exit")
+print("3. Search Student")
+print("4. Delete Student")
+print("5. Exit")
 
 while(True):
     choice = input("Enter choice : ")
@@ -41,5 +43,27 @@ while(True):
         print(students)
 
     elif choice == '3':
+       search_student = input("Enter student name to search : ")
+       flag = False
+       for student in students :
+           if search_student == student["name"] : 
+                print("student found successfully!")
+                print("name : ", student["name"])
+                print("marks : ", student["marks"])
+                flag = True
+    
+       if flag == False :
+            print("student not found")
+
+    elif choice == '4':
+        delete_student = input("Enter student to delete : ")
+        for student in students :
+            if delete_student == student["name"] :
+                students.remove(student)
+                print("student deleted successfully!")
+
+    elif choice == '5':
         print("Exit....")
         break
+
+ 
